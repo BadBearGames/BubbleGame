598682fa3634e197171f676df25c8c86deeb18c7
TIMOTHY PAZDZIORNY (RIT Student) <tkp7640@ad.rit.edu>
2016-03-15 14:52:27 -0400

    Added a simple tethering between the bubble test object and a new tether object, still needs some work so that it does not bob around

---
+++ Bubble Game/Assets/Scripts/Tether.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Transform))]
+[RequireComponent (typeof (Transform))]
+[RequireComponent (typeof (Rigidbody))]
+public class Tether : MonoBehaviour {
+
+    
+    private float length;
+
+    //Assigned in inspector
+    public Transform body;
+    public Rigidbody rbody;
+    public Transform tetherPoint;
+
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float distance = Vector3.Distance(body.position, tetherPoint.position);
+
+        if (distance > 2) {
+            Vector3 force =  tetherPoint.position - body.position;
+            rbody.AddForce(force * GameManager.Instance.bubbleRiseRate);
+        }
+        
+	}
+}
