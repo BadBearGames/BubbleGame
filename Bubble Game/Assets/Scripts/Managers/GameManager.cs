6bbde842a4b054eb204d4f28d6aaf329eef582ec
Nelson Scott <officialbadbeargames@gmail.com>
2016-03-15 12:07:58 -0400

    Organized base code

---
+++ Bubble Game/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum GameState
+{
+	None
+}
+
+public class GameManager : Singleton<GameManager> 
+{
+	#region Fields
+	//Private
+	private GameState state;
+	private float stateTimer;
+
+	//Assigned in inspector
+	public Player player;
+	#endregion
+
+	#region Properties
+	public GameState State { get { return state; } }
+	#endregion
+
+
+	protected GameManager() {}
+
+	void Awake()
+	{
+		state = GameState.None;
+	}
+
+	public void Init()
+	{
+		
+	}
+
+	void Update()
+	{
+		//Switch gamestate using a timer
+		if (stateTimer > 0f)
+		{
+			stateTimer -= Time.deltaTime;
+		}
+		else
+		{
+			//Switch state or something
+			switch (state)
+			{
+			case GameState.None:
+				SwitchGameState(GameState.None);
+				break;
+			}
+		}
+	}
+
+	/// <summary>
+	/// For switching gameplay logic
+	/// </summary>
+	/// <param name="state">State.</param>
+	public void SwitchGameState(GameState state)
+	{
+		switch (state)
+		{
+		case GameState.None:
+			//Do something here
+			break;
+		}
+		this.state = state;
+	}
+}
+
---
+++ Bubble Game/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InputManager : Singleton<InputManager> 
+{
+	//Use this class to process input
+	protected InputManager() {}
+
+	void Update()
+	{
+		//Process input here
+		//Call things like GameManager.Instance.player.Move(Direction.up) or something based on current input
+		//Place for any user interaction except ui
+	}
+}
