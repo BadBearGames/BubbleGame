fd73baa3fcfb085aa4a2b02987ce77f2c0e9986f
PTGigi <vrh4253@ad.rit.edu>
2016-03-15 13:55:36 -0400

    Player Movement

--- Bubble Game/Assets/Scripts/Managers/InputManager.cs
+++ Bubble Game/Assets/Scripts/Managers/InputManager.cs
@@ -2,16 +2,36 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class InputManager : Singleton<InputManager> 
+public class InputManager : Singleton<InputManager>
 {
 	//Use this class to process input
 	protected InputManager() {}
 
+	//Assigned in inspector
+	public GameManager gameManager;
+
 	void Update()
 	{
 		//Process input here
-		//Call things like GameManager.Instance.player.Move(Direction.up) or something based on current input
+		//Call things like GameManager.player.Move(Direction.up) or something based on current input
 		//Place for any user interaction except ui
 
+
+		/** PLAYER MOVEMENT **/
+
+		//Move left to right
+		//Movement is done with WASD/Arrow Keys/Joystick
+		if(Input.GetAxis("Horizontal")>0){
+			gameManager.player.Move(Vector3.right);
+		} else if(Input.GetAxis("Horizontal")<0){
+			gameManager.player.Move(Vector3.left);
+		}
+
+		//Jump
+		//Jump by hitting space, W, or the up arrow
+		if(gameManager.player.canJump && (Input.GetKeyDown("space") || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))){
+			gameManager.player.Jump();
+			gameManager.player.canJump = false;
+		}
 	}
 }
--- Bubble Game/Assets/Scripts/Player.cs
+++ Bubble Game/Assets/Scripts/Player.cs
@@ -2,14 +2,54 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
+	
+	//Assigned in inspector
+	public float speed;
+	public float jumpHeight;
+
+	//Boolean to prevent multiple jumps
+	public bool canJump;
+
+	//Rigidbody of Player
+	Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-	
+		//assign rigidbody
+		rb = GetComponent<Rigidbody>();
+
+		//We do not want a negative or 0 speed!
+		if(speed<0){
+			speed = .1f;
+		}
+		//We do not want a negative or 0 jump height!
+		if(jumpHeight<0){
+			jumpHeight = 1.0f;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	//Move Function
+	//Player Moves Left or Right when A/D, Left/Right Arrow, or Joystick is used
+	public void Move(Vector3 movement){
+		transform.Translate(movement*speed);
+	}
+
+	//Jump Function
+	//Player Jumps with Space/W/Up Arrow is hit
+	public void Jump(){
+		rb.velocity = new Vector3 (rb.velocity.x, jumpHeight, rb.velocity.z);
+	}
+
+	//Function to handle collision detection
+	void OnCollisionEnter(Collision collision){
+		//If we hit a platform, we can jump again!
+		if(collision.gameObject.tag == "Platform"){
+			canJump = true;
+		}
 	}
 }
