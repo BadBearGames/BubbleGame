99472ecc7aaa41a8d0e7408ace7d10bd878e8a0b
Erin Logan <ecl3369@g.rit.edu>
2016-03-15 18:50:44 -0400

    Added Respawning in Main Scene
    I went into the Player Code and built off of Gigi's platforming stuff.
    If a Player touches a Hazard, it's set to teleport to a spawn point we
    can set in the editor. The Hazard class doesn't really do anything yet,
    but it is set up if we want to play with colliding different "hazards"
    or objects. I also moved the Hazard material into the right folder. I
    think I forgot to do that in lab, but it's fixed now.

--- Bubble Game/Assets/Scripts/Hazard.cs
+++ Bubble Game/Assets/Scripts/Hazard.cs
@@ -1,14 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class Hazard : Actor {
+public class Hazard : MonoBehaviour {
 
-	bool isHit; 
 
 	// Use this for initialization
 	void Start () {
-	
-		isHit = false; 
+
 	}
 	
 	// Update is called once per frame
@@ -18,13 +16,8 @@ public class Hazard : Actor {
 
 	//when this object collides with another object
 	void OnCollisionEnter(Collision col){
-		
-		isHit = true; 
-		Debug.Log ("isHit: " + isHit); //hits
 
-		if(col.gameObject.name == "Player"){
-
-		    Debug.Log ("A player stepped on me."); //not hit
+		if(col.gameObject.tag == "Player"){
 		}
 
 	} //end of OnCollisionEnter
@@ -32,10 +25,6 @@ public class Hazard : Actor {
 
 	//whent his object stops colliding with another object
 	void OnCollisionExit(Collision col){
-
-		isHit = false; 
-		Debug.Log ("isHit: " + isHit); //hits
-
 	}//end of OnCollisionEnd
 
 }
